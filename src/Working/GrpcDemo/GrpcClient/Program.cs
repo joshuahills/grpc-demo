9003ce9c1c6cc80c3ed7028d8c07a0db0ca7b52c
Josh Hills <joshua.hills@telent.com>
2024-02-05 14:36:43 +0000

    add docker orchestration and round robin client-side load balancing to demo

--- src/Working/GrpcDemo/GrpcClient/Program.cs
+++ src/Working/GrpcDemo/GrpcClient/Program.cs
@@ -1,26 +1,51 @@
 ﻿namespace GrpcClient;
 
 using Grpc.Net.Client;
+using Grpc.Net.Client.Balancer;
+using Grpc.Net.Client.Configuration;
 using GrpcEmailerClient;
+using Microsoft.Extensions.DependencyInjection;
 
 internal class Program
 {
     static async Task Main()
     {
-        using var channel = GrpcChannel.ForAddress("http://localhost:5158");
+        var factory = new StaticResolverFactory(_ =>
+        [
+            new BalancerAddress("localhost", 5158),
+            new BalancerAddress("localhost", 32769)
+        ]);
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddSingleton<ResolverFactory>(factory);
+
+        using var channel = GrpcChannel.ForAddress(
+            "static://localhost",
+            new GrpcChannelOptions
+            {
+                Credentials = Grpc.Core.ChannelCredentials.Insecure,
+                ServiceConfig = new ServiceConfig
+                {
+                    LoadBalancingConfigs = { new RoundRobinConfig() },
+                },
+                ServiceProvider = serviceCollection.BuildServiceProvider(),
+            });
         var client = new Email.EmailClient(channel);
 
-        Console.Write("\nEnter the email address: ");
-        var address = Console.ReadLine();
+        while (true)
+        {
+            Console.Write("\nEnter the email address: ");
+            var address = Console.ReadLine();
 
-        Console.Write("Enter the subject: ");
-        var subject = Console.ReadLine();
+            Console.Write("Enter the subject: ");
+            var subject = Console.ReadLine();
 
-        Console.Write("Enter the message: ");
-        var message = Console.ReadLine();
+            Console.Write("Enter the message: ");
+            var message = Console.ReadLine();
 
-        var reply = await client.SendEmailAsync(new EmailRequest{ Address = address, Subject = subject, Content = message });
+            var reply = await client.SendEmailAsync(new EmailRequest{ Address = address, Subject = subject, Content = message });
 
-        Console.WriteLine(reply);
+            Console.WriteLine(reply);
+        }
     }
 }
